20136110c13bf19e7154c6359af95af79fe49114
jitechpc3 <you@example.com>
2021-06-07 17:27:54 +0400

    FINISH

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,7 @@ import time
 import math
 browser = webdriver.Chrome()
 def test_guest_can_add_product_to_basket():
-    browser.get('http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear')
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019')
     browser.implicitly_wait(5)
     el = browser.find_element_by_id('add_to_basket_form')
     el.click() 
@@ -13,7 +13,8 @@ def test_guest_can_add_product_to_basket():
     answer = str(math.log(abs((12 * math.sin(float(x))))))
     alert.send_keys(answer)
     alert.accept()
-    time.sleep(3)
+    time.sleep(5)
+    browser.quit()
 
 
 
