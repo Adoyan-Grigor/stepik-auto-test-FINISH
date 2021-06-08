423989f773e573c79dd66b4d04020857b70e6ba0
jitechpc3 <you@example.com>
2021-06-08 17:12:09 +0400

    FINISH

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 from selenium import webdriver
+import pytest
 import time
 import math
 browser = webdriver.Chrome()
-def test_guest_can_add_product_to_basket():
-    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019')
+
+@pytest.mark.parametrize('link', ["0", "1","2", "3", "4", "5", "6", "7", "8", "9"])
+
+def test_guest_can_add_product_to_basket(browser, link):
+    lk = ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}")
+    browser.get(lk)
     browser.implicitly_wait(5)
     el = browser.find_element_by_id('add_to_basket_form')
     el.click() 
@@ -13,8 +18,10 @@ def test_guest_can_add_product_to_basket():
     answer = str(math.log(abs((12 * math.sin(float(x))))))
     alert.send_keys(answer)
     alert.accept()
-    time.sleep(5)
+    time.sleep(1)
     browser.quit()
+
+
 
 
 
